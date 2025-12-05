5e77256a38dc850ca23dfa86f06ccdb8366d01ed
Schweizer_KL <schweizer23.ss@gmail.com>
2025-12-05 11:06:28 +0800

    remove *

--- server.js
+++ server.js
@@ -124,9 +124,9 @@ app.get("/api/boat/history", async (req, res) => {
 });
 
 // Fallback to index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
